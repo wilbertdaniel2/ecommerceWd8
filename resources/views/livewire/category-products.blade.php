6e734f91d18536b5616dd14823e067cdd322f5b3
unknown <wilberdaniel2@gmail.com>
2023-10-19 04:12:27 -0500

    Se hace cambio en las tarjetas de los productos para que sea responsivo en la vista de moviles

--- resources/views/livewire/category-products.blade.php
+++ resources/views/livewire/category-products.blade.php
@@ -2,11 +2,10 @@
     @if (count($products))
 
         <div class="glider-contain">
-            <ul class="glider-{{$category->id}}">
-                
+            <ul class="glider-{{ $category->id }}">
+
                 @foreach ($products as $product)
-                    
-                <li class="bg-white rounded-lg shadow {{ $loop->last ? '' : 'sm:mr-4' }}">
+                    {{-- <li class="bg-white rounded-lg shadow {{ $loop->last ? '' : 'sm:mr-4' }}">
                         
                     <article>
                         <a href="{{route('products.show', $product)}}">
@@ -18,36 +17,41 @@
                             <div class="py-4 px-6">
                                 <h1 class="text-lg font-semibold">
                                     <a href="{{route('products.show', $product)}}">
-                                        {{Str::limit($product->name, 20)}} {{--Con esta linea de codigo estoy limitando a que solo se muestre los primeros 20 caracteres del nombre del producto--}}
+                                        {{Str::limit($product->name, 20)}} {{-- Con esta linea de codigo estoy limitando a que solo se muestre los primeros 20 caracteres del nombre del producto
                                     </a>
                                 </h1>
                                 <p class="font-bold text-truegray">COP {{number_format($product->price)}}</p>
                             </div>        
                     </article>
-                </li>
+                    </li> --}}
 
+                    <a href="{{ route('products.show', $product) }}" class="mr-4">
+                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                            <img src="{{ Storage::url($product->images->first()->url) }}"
+                                alt=""
+                                class="h-full w-full object-cover object-center group-hover:opacity-75">
+                        </div>
+                        <h3 class="mt-4 text-lg font-semibold text-gray-700">{{ Str::limit($product->name, 20) }}</h3>
+                        <p class="mt-1 text-lg font-medium text-gray-900">$ {{ number_format($product->price) }}</p>
+                    </a>
                 @endforeach
             </ul>
-        
+
             <button aria-label="Previous" class="glider-prev">«</button>
             <button aria-label="Next" class="glider-next">»</button>
-            {{--Con esta linea de codigo muestro los puntos debajo de las imagenes--}}
+            {{-- Con esta linea de codigo muestro los puntos debajo de las imagenes --}}
             {{-- <div role="tablist" class="dots"></div> --}}
         </div>
-
     @else
-        {{--Spiner de carga, puede ser modificado en un futuro dependiendo del gusto--}}
+        {{-- Spiner de carga, puede ser modificado en un futuro dependiendo del gusto --}}
         <div class="flex items-center justify-center">
-            <div
-              class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-              role="status">
-              <span
-                class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                >Loading...</span
-              >
+            <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status">
+                <span
+                    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
             </div>
-          </div>	    
+        </div>
 
     @endif
- 
+
 </div>
