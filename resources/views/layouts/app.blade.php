aafa3c6e8982b74ac7a5466589467906f97db648
unknown <wilbertdaniel2@gmail.com>
2024-06-05 09:29:23 -0500

    se agrega etiqueta de google merchants

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+    <meta name="google-site-verification" content="tFzSynmzKvNuE7DNv6zQTP_hWvfJ4MQBMpCgvP7uD1E" />
+
     {{-- swipe --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
