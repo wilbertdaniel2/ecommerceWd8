5cbfae82bcf416484db72b19f7bc0a1f2a1eb745
Wilbertdaniel2 <wilbertdaniel2@gmail.com>
2024-07-05 09:51:11 -0500

    se agrega pixel de facebook

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,33 @@
 
     <meta name="google-site-verification" content="tFzSynmzKvNuE7DNv6zQTP_hWvfJ4MQBMpCgvP7uD1E" />
 
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1011423373679681');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1011423373679681&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
+
     {{-- swipe --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
@@ -258,7 +285,8 @@
             <span>© 2024 Copyright:</span>
             <a class="font-semibold text-neutral-200 dark:text-neutral-400 hover:text-rojo-600"
                 href="https://diswelldone.com/">Welldone</a>
-                <div class="mt-8 text-sm">Designed by <a class="hover:text-rojo-600" href="https://github.com/wilbertdaniel2">Wilbert Avila F</a></div>
+            <div class="mt-8 text-sm">Designed by <a class="hover:text-rojo-600"
+                    href="https://github.com/wilbertdaniel2">Wilbert Avila F</a></div>
         </div>
     </footer>
 </body>
