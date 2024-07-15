940b5c419c755f5d6c73b873346ec3082d75e4ad
Wilbertdaniel2 <wilbertdaniel2@gmail.com>
2024-07-15 12:57:51 -0500

    se corrige la visualizacion de las caracteristicas en la vista celular

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -212,11 +212,11 @@
                     <li class="pb-3 sm:pb-4">
                        <div class="flex items-center space-x-4 rtl:space-x-reverse">
                           <div class="flex-1 min-w-0">
-                             <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                             <p class="text-sm font-medium text-gray-900 truncate">
                                 {{ $feature->feature->name }}
                              </p>
                           </div>
-                          <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                          <div class="inline-flex items-center text-base font-semibold text-gray-900">
                             {{ $feature->description }}
                           </div>
                        </div>
